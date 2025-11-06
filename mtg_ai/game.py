480a94dd24eb348615d2fc107700cd2a0b8bfd84
ariel610 <ariel610@proton.me>
2025-11-06 18:04:20 -0500

    automatically makes a copy when cards change zones

--- mtg_ai/game.py
+++ mtg_ai/game.py
@@ -1,25 +1,90 @@
 from dataclasses import dataclass
-from typing import TypeVar, Optional, List
+from typing import TypeVar, Optional, List, Tuple, Dict, Any, Iterable
+from collections.abc import Callable
 from collections import defaultdict
 from enum import Enum
 
 Player = int
-Card = TypeVar('Card')
 StackObject = TypeVar('StackObj')
-
-
-@dataclass
-class GameState: 
-    players: List[Player]
-    cards: List[Card]
-    stack: List[StackObject]
-    active: Player
-    priority: Player
+CardRef = int
 
 @dataclass
 class Zone:
     owner: Optional[Player] = None
     position: Optional[int] = None
+
+    def contains(self, card):
+        return (
+            type(self) is type(card.zone) 
+            and self.owner == card.zone.owner
+            and (self.position is None or self.position == card.zone.position) 
+        )
+    
+    def __str__(self):
+        return f"{type(self)}({self.owner})[{self.position}]"
+
+    def copy(self):
+        return type(self)(self.owner, self.position)
+
+class Game:
+    def __init__(self, decks: List[List['Card']], track_history: bool = False):
+        self.history = []
+        players = list(range(len(decks)))
+        for p in players:
+            deck = decks[p]
+            for position in range(len(deck)):
+                deck[position].zone = Deck(owner=p, position=position)
+        cards = [c for deck in decks for c in deck]
+        self.current = GameState(
+            players, cards, [],
+            0,0
+        )
+        if track_history:
+            ChangeTracker.on_change(self.on_change)
+
+    def on_change(self, _):
+        prev_gamestate = self.current.copy()
+        self.history.append(prev_gamestate)
+
+    @property
+    def active(self):
+        return self.current.active
+
+    @property
+    def priority(self):
+        return self.current.priority
+
+    def draw(self, player: Player):
+        deck = self.current.get_zone(Deck(owner=player))
+        top_card = deck.pop()
+        top_card.zone = Hand(owner=player)
+
+    def play(self, card):
+        card.zone = Field(owner=card.zone.owner)
+
+    def cast(self, card):
+        card.zone = Stack(owner=None,position=len(self.current.stack))
+        self.current.stack.append(card)
+
+
+@dataclass
+class GameState: 
+    players: List[Player]
+    cards: List['Card']
+    stack: List[StackObject]
+    active: int
+    priority: int
+    
+    def copy(self):
+        # this is probably too neat
+        cards = [card.copy() for card in self.cards]
+        stack = [obj.copy() for obj in self.stack]
+
+        return GameState(self.players, cards, stack, self.active, self.priority)
+
+    def get_zone(self, zone: Zone):
+        return sorted([c for c in self.cards if zone.contains(c)],
+        key=lambda card: card.zone.position)
 
 class Grave(Zone):
     pass
@@ -36,45 +101,73 @@ class Field(Zone):
 class Stack(Zone):
     pass
 
-class ZoneTracker:
+class Any(Zone):
+    pass
 
-    def __init__(self):
-        self._zone = None
+class ChangeTracker:
 
-    handlers = defaultdict(list)
+    changes: Dict[Tuple[Zone,Zone],Callable[[Any,Zone,Zone],None]] = defaultdict(list)
+    change_from = defaultdict(list)
+    change_to = defaultdict(list)
 
-    def __get__(self, obj):
-        return self._zone
+    def __set_name__(self,owner, name):
+        self.private = f"_{name}"
 
-    def __set__(self, obj, new_zone):
-        if new_zone != self._zone:
-            for callback in ZoneTracker.handlers[(type(self._zone),type(new_zone))]:
-                callback(obj, self._zone, new_zone)
-        self._zone = new_zone
+    def __get__(self, obj, _objtype):
+        return getattr(obj,self.private)
+
+    def __set__(self, obj, value):
+        old_value = getattr(obj, self.private,None)
+        if value != old_value:
+            for callback in ChangeTracker.changes[(type(old_value),type(value))]:
+                callback(obj, obj._zone, value)
+            for callback in (
+                    ChangeTracker.change_from[type(old_value)] + 
+                    ChangeTracker.change_from[Any] + 
+                    ChangeTracker.change_to[type(value)]):
+                callback(obj)
+        setattr(obj,self.private,value)
 
     @classmethod
     def on_draw(cls, callback):
-        cls.handlers[(Deck, Hand)].append(callback)
+        cls.changes[(Deck, Hand)].append(callback)
     
     @classmethod
     def on_discard(cls, callback):
-        cls.handlers[(Hand,Grave)].append(callback)
+        cls.changes[(Hand,Grave)].append(callback)
 
     @classmethod
     def on_cast(cls, callback):
-        cls.handlers[(Hand, Stack)].append(callback)
+        cls.changes[(Hand, Stack)].append(callback)
 
     @classmethod
     def on_enters(cls, callback): 
-        for zone in (Hand, Deck, Grave, Stack):
-            cls.handlers[(zone,Field)].append(callback)
+        cls.change_to[Field].append(callback)
 
-class Card: 
-    zone = ZoneTracker()
+    @classmethod
+    def on_change(cls, callback):
+        cls.change_from[Any].append(callback)
 
-    def when_drawn(self, _a, _b):
-        print(f"Drew {self.name}!")
+class CardType(str, Enum):
+    Land = "land"
+    Creature = "creature"
 
-    def __init__(self,name):
+class Card:
+    zone = ChangeTracker()
+
+    def __init__(self,name, types: Iterable[CardType]=(), zone:Optional[Zone]=None):
         self.name = name
-        ZoneTracker.on_draw(Card.when_drawn)
+        self.types = set(types)
+        # need to dodge the change tracker here
+        self.__dict__['zone'] = zone
+
+    def copy(self):
+        return Card(self.name, self.types, self.zone.copy())
+
+    def __str__(self):
+        return f"[{self.name}@{self.zone}]"
+
+    def __repr__(self):
+        return str(self)
+
+Forest = Card("Forest",[CardType.Land])
--- tests/test_watcher.py
+++ tests/test_watcher.py
@@ -1,7 +1,7 @@
 import mtg_ai.game as mtg
 
 def test_watcher():
-    prev = len(mtg.ZoneTracker.handlers[(mtg.Deck, mtg.Hand)])
+    prev = len(mtg.ChangeTracker.changes[(mtg.Deck, mtg.Hand)])
     class Checker:
         def __init__(self):
             self.called = False
@@ -9,8 +9,8 @@ def test_watcher():
         def __call__(self, *args):
             self.called = True
     checker = Checker()
-    mtg.ZoneTracker.on_draw(checker)
-    new = len(mtg.ZoneTracker.handlers[(mtg.Deck, mtg.Hand)])
+    mtg.ChangeTracker.on_draw(checker)
+    new = len(mtg.ChangeTracker.changes[(mtg.Deck, mtg.Hand)])
     assert prev + 1 == new
 
     card = mtg.Card("Glass")
@@ -18,3 +18,21 @@ def test_watcher():
     card.zone = mtg.Hand()
 
     assert checker.called
+
+
+def test_deck():
+    deck = [[mtg.Card(name) for name in 'ABCDE']]
+    game = mtg.Game(decks=deck, track_history=True)
+    assert game.current.players == [0]
+    
+    deck_zone = mtg.Deck(owner=0)
+    new_deck = game.current.get_zone(deck_zone)
+    assert len(new_deck) == len(deck[0])
+
+def test_history():
+    deck = [mtg.Card(name) for name in ('a','b','c','d','e')]
+    game = mtg.Game(decks=[deck], track_history=True)
+    for i in range(3):
+        game.draw(0)
+        assert len(game.history) == i+1
+        assert len(game.current.get_zone(mtg.Deck(owner=0))) == len(deck) - (i+1)
